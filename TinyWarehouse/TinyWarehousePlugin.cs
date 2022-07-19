1a497540418f927d522fc04967e6ca557ca2f6a0
Hytone <hannu_94@hotmail.com>
2022-07-19 21:26:09 +0300

    Removed unnecessary log messages

--- TinyWarehouse/TinyWarehousePlugin.cs
+++ TinyWarehouse/TinyWarehousePlugin.cs
@@ -6,7 +6,7 @@ using TimberbornAPI.Common;
 
 namespace Hytone.TinyWarehouse
 {
-    [BepInPlugin("hytone.plugins.tinywarehouse", "TinyWarehouse", "1.0.1")]
+    [BepInPlugin("hytone.plugins.tinywarehouse", "TinyWarehouse", "1.0.2")]
     [BepInDependency("com.timberapi.timberapi")]
     [HarmonyPatch]
     public class TinyWarehousePlugin : BaseUnityPlugin
